42a21fc5b6200a647ff5afa522176af13ac1fee5
Haruka Ogino <harukariq@gmail.com>
2024-04-25 23:42:06 +1200

    further style projects

--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -10,10 +10,47 @@ export default function Projects() {
           />
           <div className="description">
             <h3>All the Ghibli Things!</h3>
+            <p>
+              A fun full-stack web application I worked on using React and
+              SQLite alongside Knex.
+              <br />
+              <br />
+              While I have accomplished a lot of my goals for this app, I am
+              still constantly working on it. Learn more about my goals on the
+              GitHub repo.
+            </p>
             <a href="https://ghibli-things.devacademy.nz/">
-              <p>Deployed web app</p>
+              <p>https://ghibli-things.devacademy.nz/</p>
             </a>
             <a href="https://github.com/haruka-ogino/Ghibli-Things">
+              <p>GitHub repository</p>
+            </a>
+          </div>
+        </div>
+        <div className="project">
+          <img
+            src="https://www.ghibli-museum.jp/en/img/mainimg.png"
+            alt="ghibli character"
+          />
+          <div className="description">
+            <h3>Lucas&apos; Mansion</h3>
+            <a href="https://lucas-mansion.pushed.nz/">
+              <p>https://lucas-mansion.pushed.nz/</p>
+            </a>
+            <a href="https://github.com/harakeke-2024/Lucas-Mansion">
+              <p>GitHub repository</p>
+            </a>
+          </div>
+        </div>
+        <div className="project">
+          <img
+            src="https://www.ghibli-museum.jp/en/img/mainimg.png"
+            alt="ghibli character"
+          />
+          <div className="description">
+            <h3>Pet Blog</h3>
+            <p>This web application is under construction.</p>
+            <a href="https://github.com/harakeke-2024/Lucas-Mansion">
               <p>GitHub repository</p>
             </a>
           </div>
