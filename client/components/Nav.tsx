16062a1fcab56658d6b4f3600f177941b7b4dac4
Haruka Ogino <harukariq@gmail.com>
2024-04-28 20:16:25 +1200

    add my external links to nav comp

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -5,9 +5,26 @@ export default function Nav() {
       heading.scrollIntoView({ behavior: 'smooth' })
     }
   }
+
+  const handleLink = (link: string) => {
+    window.open(link, '_blank')
+  }
+
   return (
     <nav id="nav">
+      <button onClick={() => handleClick('about')}>About Me</button>
       <button onClick={() => handleClick('projects')}>Projects</button>
+      <button onClick={() => handleLink('/Haruka-Ogino-CV.pdf')}>My CV</button>
+      <button onClick={() => handleLink('/https://github.com/haruka-ogino')}>
+        My GitHub
+      </button>
+      <button
+        onClick={() =>
+          handleLink('https://nz.linkedin.com/in/haruka-ogino-769540148')
+        }
+      >
+        My LinkedIn
+      </button>
     </nav>
   )
 }
