2057dc5a1813c3dd861d13afced69377cf781896
Haruka Ogino <harukariq@gmail.com>
2024-05-14 10:21:28 +1200

    create blog route

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,13 +1,15 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import App from './components/App'
 import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
+// import About from './components/About'
+// import Projects from './components/Projects'
+import Blog from './components/Blog'
 
 export default createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Home />} />
     {/* <Route path="about" element={<About />} /> */}
     {/* <Route path="projects" element={<Projects />} /> */}
+    <Route path="blog" element={<Blog />} />
   </Route>
 )
