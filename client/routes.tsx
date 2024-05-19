68d7cd5d4075174591d7e7f0ce6337107800f517
Haruka Ogino <harukariq@gmail.com>
2024-05-19 21:59:49 +1200

    comment out some routes

--- client/routes.tsx
+++ client/routes.tsx
@@ -3,15 +3,15 @@ import App from './components/App'
 import Home from './components/Home'
 // import About from './components/About'
 // import Projects from './components/Projects'
-import Blog from './components/Blog'
-import BlogEntry from './components/BlogEntry'
+// import Blog from './components/Blog'
+// import BlogEntry from './components/BlogEntry'
 
 export default createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Home />} />
     {/* <Route path="about" element={<About />} /> */}
     {/* <Route path="projects" element={<Projects />} /> */}
-    <Route path="blog" element={<Blog />} />
-    <Route path="blog/:entry" element={<BlogEntry />} />
+    {/* <Route path="blog" element={<Blog />} /> */}
+    {/* <Route path="blog/:entry" element={<BlogEntry />} /> */}
   </Route>
 )
