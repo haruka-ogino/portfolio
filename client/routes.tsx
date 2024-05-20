54313d1c48cb47f0c78c16a313d13d84b22f1bc9
Haruka Ogino <harukariq@gmail.com>
2024-05-20 21:22:34 +1200

    tidy up routes

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,16 +1,12 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import App from './components/App'
 import Home from './components/Home'
-// import About from './components/About'
-// import Projects from './components/Projects'
 import Blog from './components/Blog'
 import BlogPage from './components/BlogPage'
 
 export default createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<Home />} />
-    {/* <Route path="about" element={<About />} /> */}
-    {/* <Route path="projects" element={<Projects />} /> */}
     <Route path="blog" element={<Blog />} />
     <Route path="blog/:entry" element={<BlogPage />} />
   </Route>
